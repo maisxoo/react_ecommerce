df5354eb8b0c0c48e0387652ef7a4032407dcab4
maisxoo <144647996+maisxoo@users.noreply.github.com>
2024-03-11 16:33:08 +0800

    32 using stateful list in react

--- ecommerce_react_procad/src/App.js
+++ ecommerce_react_procad/src/App.js
@@ -3,17 +3,66 @@ import ProductList from './components/ProductList/ProductList';
 import CreateProduct from './components/CreateProducts/CreateProduct';
 
 import './App.css';
-
+const products = [
+	{
+		pId: 1,
+		pName: 'Arla',
+		desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing ipsum dolor sit amet.',
+		isAvailable: true,
+		image: 'images/arla.png',
+		price: 15
+	},
+	{
+		pID: 2,
+		pName: 'Fresh Milk',
+		desc: 'Lorem ipsum keme keme keme 48 years mashumers pinkalou shontis ano lulu ng lulu kasi ugmas bakit.',
+		isAvailable: true,
+		image: 'images/fresh-milk.png',
+		price: 15
+	},
+	{
+		pID: 3,
+		pName: 'Nestle Non-Fat Milk ',
+		desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+		isAvailable: true,
+		image: 'images/nestle-nonfat.png',
+		price: 18
+	},
+	{
+		pID: 4,
+		pName: 'Jolly Cow',
+		desc: 'Lorem ipsum keme keme keme 48 years mabaho sudems shala na ang ng balaj dites lorem ipsum keme.',
+		isAvailable: false,
+		image: 'images/cow-head.png',
+		price: 14
+	},
+	{
+		pID: 5,
+		pName: 'Bear Brand',
+		desc: 'Lorem ipsum keme keme keme 48 years na tetetet bakit intonses tanders bokot neuro.',
+		isAvailable: true,
+		image: 'images/bear-brand.png',
+		price: 16
+	},
+	{
+		pID: 6,
+		pName: 'Emborg',
+		desc: 'Lorem ipsum keme keme keme 48 years na tetetet bakit intonses tanders bokot neuro.',
+		isAvailable: true,
+		image: 'images/emborg.png',
+		price: 16
+	}
+];
 function App() {
-	let [newProduct, updateProduct] = useState(null);
+	let [newProductList, updateProductList] = useState(products);
 
 	function createProduct(product) {
-		updateProduct(product);
+		updateProductList([product], ...newProductList);
 	}
 	return (
 		<>
 			<CreateProduct createProduct={createProduct} />
-			<ProductList newProduct={newProduct} />
+			<ProductList newProductList={newProductList} />
 		</>
 	);
 }
--- ecommerce_react_procad/src/components/ProductList/ProductList.js
+++ ecommerce_react_procad/src/components/ProductList/ProductList.js
@@ -1,72 +1,25 @@
+import React, {useState} from 'react';
 import Products from './Products';
-const products = [
-	{
-		pId: 1,
-		pName: 'Arla',
-		desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing ipsum dolor sit amet.',
-		isAvailable: true,
-		image: 'images/arla.png',
-		price: 15
-	},
-	{
-		pID: 2,
-		pName: 'Fresh Milk',
-		desc: 'Lorem ipsum keme keme keme 48 years mashumers pinkalou shontis ano lulu ng lulu kasi ugmas bakit.',
-		isAvailable: true,
-		image: 'images/fresh-milk.png',
-		price: 15
-	},
-	{
-		pID: 3,
-		pName: 'Nestle Non-Fat Milk ',
-		desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
-		isAvailable: true,
-		image: 'images/nestle-nonfat.png',
-		price: 18
-	},
-	{
-		pID: 4,
-		pName: 'Jolly Cow',
-		desc: 'Lorem ipsum keme keme keme 48 years mabaho sudems shala na ang ng balaj dites lorem ipsum keme.',
-		isAvailable: false,
-		image: 'images/cow-head.png',
-		price: 14
-	},
-	{
-		pID: 5,
-		pName: 'Bear Brand',
-		desc: 'Lorem ipsum keme keme keme 48 years na tetetet bakit intonses tanders bokot neuro.',
-		isAvailable: true,
-		image: 'images/bear-brand.png',
-		price: 16
-	},
-	{
-		pID: 6,
-		pName: 'Emborg',
-		desc: 'Lorem ipsum keme keme keme 48 years na tetetet bakit intonses tanders bokot neuro.',
-		isAvailable: true,
-		image: 'images/emborg.png',
-		price: 16
-	}
-];
+
 function ProductList(props) {
-	console.log(props.newProduct);
+	// console.log(props.newProduct);
+	// let [newProductList, updateProductList] = useState(products);
+	// updateProductList([props.newProduct, ...products]);
 	return (
 		<div className='row'>
 			<div className='col-lg-8 mx-auto'>
 				<ul className='list-group shadow'>
-					{products.map(product => {
-						return (
-							<Products>
-								id={product.pId}
-								name={product.pName}
-								description={product.desc}
-								isAvailable={product.isAvailable}
-								image={product.image}
-								price={product.price}
-							</Products>
-						);
-					})}
+					{props.newProductList.map(product => (
+						<Products
+							key={product.pId}
+							id={product?.pId}
+							name={product?.pName}
+							description={product?.desc}
+							isAvailable={product?.isAvailable}
+							image={product?.image}
+							price={product?.price}
+						/>
+					))}
 				</ul>
 			</div>
 		</div>
